e978b27caa8f405dc1cc22fd9d2372ff7d517715
Jessica <jessicakoch136@icloud.com>
2015-06-16 16:17:43 -0600

    remove api

--- api/app/api.rb
+++
@@ -1,8 +0,0 @@
-# lib/api.rb
-
-require 'sinatra/activerecord'
-Dir["app/lib/**/*.rb"].each{ |f| require File.absolute_path(f)}
-
-class PasswebAPI < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-end
