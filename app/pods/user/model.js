3c6675838ae0680cd3d214b3a238c5be9e3ad732
Jessica <jessicakoch136@icloud.com>
2015-06-17 15:07:54 -0600

    add user model

--- app/pods/user/model.js
+++ app/pods/user/model.js
@@ -1,5 +1,24 @@
+// import the default value from ember-data into the variable DS.
+// 
+// Ember-Data exprts by default a namespace (known as DS) that exposes all the 
+// classes and functions defined in http://emberjs.com/api/data
+
 import DS from 'ember-data';
+// Define the default export for this model, which will be a subclass 
+// of DS.Model
+// 
+// After this class has been defined, we can import tis subclass doing:
+// import User from 'fetch/models/user'
+// 
+// We can also use relative imports.  So if we were in another model we
+// could have written
+// import User from './user';
 
 export default DS.Model.extend({
-  
+  // Ds.attr is the standard way to define attributes with Ember-Data
+  firstName: DS.attr('string'),
+
+  // Defines an attribute called lastName of type **string**
+  lastName: DS.attr('string'),
+  email: DS.attr('string')
 });
