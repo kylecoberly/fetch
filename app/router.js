19915e697c43b2bd27d1ba245ad8bc4e11544ca4
Jessica <jessicakoch136@icloud.com>
2015-06-17 16:43:57 -0600

    add user index route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('users');
+  this.resource('users', function(){ });
+  this.route('users', function() {});
 });
 
 export default Router;
