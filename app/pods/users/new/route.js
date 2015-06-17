d2e82b22f5569071f0a082d11ffd7ef6c4d9cbb1
Jessica <jessicakoch136@icloud.com>
2015-06-17 17:54:47 -0600

    add new user form

--- app/pods/users/new/route.js
+++ app/pods/users/new/route.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
+    //store is Ember-Data class in charge of managaging 
+    // everthing related to model's data
     return this.store.createRecord('user');
   }
 });
