b4bc6d61a39f1240e85249724a967933dac60eca
Jessica <jessicakoch136@icloud.com>
2015-06-17 15:57:14 -0600

    update environment file

--- app/pods/users/route.js
+++ app/pods/users/route.js
@@ -1,4 +1,12 @@
+// Assigns he default export from **ember** into the variable Ember.
+// 
+// The default export for the ember package is a namespace that
+// contains all the classes and functions for Ember that are specified in
+// http://emberjs.com/api/
 import Ember from 'ember';
 
+// Defines teh default export or this module.  For now we will not 
+// add anything extra, but if we want to use a Route **hook** or 
+// **actions** this would be the place
 export default Ember.Route.extend({
 });
