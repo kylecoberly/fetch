171bd40cd4d1a96b07b6fb5ac93379170cd34e27
Jessica <jessicakoch136@icloud.com>
2015-06-16 23:25:05 -0600

    make an updated clock with controller

--- app/pods/clock/controller.js
+++ app/pods/clock/controller.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
 // controller name should be the same as model and route
 export default Ember.Controller.extend({
+  init: function(){
+    // Update the time
+    this.updateTime();
+  },
+
+  updateTime: function(){
+    let _this = this;
+
+    // Update the time every second
+    Ember.run.later(function(){
+      _this.set('localTime', new Date().toLocaleTimeString());
+      _this.updateTime();
+    }, 1000);
+  },
+  
   localTime: new Date().toLocaleTimeString()
 });
