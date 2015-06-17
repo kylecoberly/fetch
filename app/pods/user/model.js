630ece3042db3aa611da0add9fc5f9c983eeb1c7
Jessica <jessicakoch136@icloud.com>
2015-06-16 21:39:42 -0600

    add user model

--- app/pods/user/model.js
+++ app/pods/user/model.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  first_name: DS.attr('string'),
+  last_name: DS.attr('string'),
+  email: DS.attr('string'),
+  password: DS.attr('string'),
+  dogs: DS.hasMany('dogs'),
 });
