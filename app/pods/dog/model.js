d1363bfe5e7a351c391c6550eb4afde19fb68410
Jessica <jessicakoch136@icloud.com>
2015-06-16 21:36:11 -0600

    update dog model

--- app/pods/dog/model.js
+++ app/pods/dog/model.js
@@ -1,5 +1,10 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  name: DS.attr('string'),
+  age: DS.attr('string'),
+  gender: DS.attr('string'),
+  breed: DS.attr('string'),
+  user: DS.belongsTo('user'),
+
 });
