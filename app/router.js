d28a55c04a80b480dac56ccb89b0d761e6999706
Jessica <jessicakoch136@icloud.com>
2015-06-18 12:37:28 -0600

    update resouce to route

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('users', function(){ 
+  this.route('users', function(){ 
     this.route('new');
     this.route('show', { path: ':user_id'});
     this.route('edit', { path: ':user_id/edit'});
