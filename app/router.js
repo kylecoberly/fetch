0a4b38b70f1fe7b3a16d58508f2981c038cc0868
Jessica <jessicakoch136@icloud.com>
2015-06-18 00:03:39 -0600

    update user template to edit users

--- app/router.js
+++ app/router.js
@@ -8,9 +8,8 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('users', function(){ 
     this.route('new');
-    this.route('show', {
-      path: ':user_id'
-    });
+    this.route('show', { path: ':user_id'});
+    this.route('edit', { path: ':user_id/edit'});
   });
 });
 
