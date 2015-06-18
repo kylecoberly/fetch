2e16ad482fe3c72dbf3a0c31c31a176e85d893bd
Jessica <jessicakoch136@icloud.com>
2015-06-17 20:43:41 -0600

    update controller and template with error message and validation

--- app/pods/users/new/controller.js
+++ app/pods/users/new/controller.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName', 
+    function(){
+      return !Ember.isEmpty(this.get('model.email')) &&
+      !Ember.isEmpty(this.get('model.firstName')) &&
+      !Ember.isEmpty(this.get('model.lastName'));
+    }),
   actions: {
     save: function(){
       console.log('+- save action in users new controller');
