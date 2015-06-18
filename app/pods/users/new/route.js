f6130aaf664ddfaca2fc3e7aa4abb45c888200b6
Jessica <jessicakoch136@icloud.com>
2015-06-17 19:28:32 -0600

    update user new route

--- app/pods/users/new/route.js
+++ app/pods/users/new/route.js
@@ -5,5 +5,17 @@ export default Ember.Route.extend({
     //store is Ember-Data class in charge of managaging 
     // everthing related to model's data
     return this.store.createRecord('user');
+  },
+  actions: {
+    save: function(){
+      console.log('+-- save action bubbled up to friends new route');
+
+      return true;
+    },
+    cance: function(){
+      console.log('+-- cancel action bubbled up to friends new route');
+
+      return true;
+    }
   }
 });
