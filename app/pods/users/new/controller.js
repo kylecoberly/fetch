71b5614fee3fa8d529b00df6701bab8bb160382a
Jessica <jessicakoch136@icloud.com>
2015-06-17 20:51:47 -0600

    update controller for user

--- app/pods/users/new/controller.js
+++ app/pods/users/new/controller.js
@@ -12,12 +12,23 @@ export default Ember.Controller.extend({
     }),
   actions: {
     save: function(){
-      console.log('+- save action in users new controller');
-      return true;
+      if (this.get('isValid')){
+        // needed because scope is different inside the function
+        var _this = this;
+        // return of .save() is a promise 
+        // .then receives a fxn that will be called when model is saved to server
+        // returns an instance and then we can go to user show page
+        this.get('model').save().then(function(user) {
+          _this.transitionToRoute('users.show', user);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+      return false;
     },
     cancel: function(){
-      console.log('+- cancel action in users new controller');
-      return true;
+      this.transitionToRoute('users');
+      return false;
     }
   }
 });
