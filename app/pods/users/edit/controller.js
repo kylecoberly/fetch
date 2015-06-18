1a765895508dd0741915dfb6856f75b97af4651b
Jessica <jessicakoch136@icloud.com>
2015-06-18 13:45:10 -0600

    add cancel and save actions to edit route

---
+++ app/pods/users/edit/controller.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName',
+    function(){
+      return !Ember.isEmpty(this.get('model.email')) &&
+      !Ember.isEmpty(this.get('model.firstName')) &&
+      !Ember.isEmpty(this.get('model.lastName'));
+    }),
+  actions: {
+    save: function(){
+      if (this.get('isValid')) {
+        var _this = this;
+        this.get('model').save().then(function(user){
+          _this.transitionToRoute('users.show', user);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+      return false;
+    },
+    cancel: function(){
+      this.transitionToRoute('users.show', this.get('model'));
+      return false;
+    }
+  }
+});
