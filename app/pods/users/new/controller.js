af7548b2b4cfc06baf0a40964e383ced71c3297e
Jessica <jessicakoch136@icloud.com>
2015-06-19 14:47:01 -0600

    update controller back to what it was

--- app/pods/users/new/controller.js
+++ app/pods/users/new/controller.js
@@ -1,10 +1,30 @@
-import UsersBaseController from './base';
+import Ember from 'ember';
 
-export default UsersBaseController.extend({
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.email',
+    'model.firstName',
+    'model.lastName', 
+    function(){
+      return !Ember.isEmpty(this.get('model.email')) &&
+      !Ember.isEmpty(this.get('model.firstName')) &&
+      !Ember.isEmpty(this.get('model.lastName'));
+    }),
   actions: {
-    cancel: function(){
-      this.transitionToRoute('users.index');
-      return false;
+    save: function() {
+      if (this.get('isValid')) { 
+        var _this = this; 
+        this.get('model').save().then(function(user) { 
+          _this.transitionToRoute('users.show', user);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields'); 
+      } 
+      return false; 
+    }, 
+    cancel: function() { 
+      this.transitionToRoute('users'); 
+      return false; 
     }
   }
 });
