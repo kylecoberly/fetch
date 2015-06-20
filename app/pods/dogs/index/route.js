524199e957193fb950ef83feea785d925c4a0f5d
Jessica <jessicakoch136@icloud.com>
2015-06-20 14:24:17 -0600

    update dog index page

--- app/pods/dogs/index/route.js
+++ app/pods/dogs/index/route.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
+    // modelFor helps grab the model for ay parent route
     return this.modelFor('users/show').get('dogs');
   }
 });
