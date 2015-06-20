844ef42a27e0ab9febe8faec77582f77ac1a59fd
Jessica <jessicakoch136@icloud.com>
2015-06-20 16:19:32 -0600

    add dog model

--- app/pods/dog/model.js
+++ app/pods/dog/model.js
@@ -7,3 +7,9 @@ export default DS.Model.extend({
   breed: DS.attr('string'),
   user: DS.belongsTo('user')
 });
+
+// can set a default value through ember data such as:
+// 
+  // breed: DS.attr('string', {
+    // defaultValue: 'Alaskan Klee Kai'
+  // })
