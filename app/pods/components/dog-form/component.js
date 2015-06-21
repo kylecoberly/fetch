29c8101504d9b9f549a4ece22033bb304ca092e3
Jessica <jessicakoch136@icloud.com>
2015-06-21 03:50:17 -0600

    edit dog component

--- app/pods/components/dog-form/component.js
+++ app/pods/components/dog-form/component.js
@@ -13,19 +13,30 @@ export default Ember.Component.extend({
       !Ember.isEmpty(this.get('dog.breed'));
     }),
   actions: {
-    save: function(){
-      if (this.get('isValid')){
-        let _this = this;
-        this.get('dog').save().then(function(dog){
-          _this.transitionToRoute('dogs.show', dog);
-        });
-      } else{
-        this.set('errorMessage', 'You have to fill all the fields');
-      }
-      return false;
+    saveDog: function(dog){
+      this.sendAction('save', dog);
     },
     cancel: function(){
       return false;
     }
   }
 });
+
+
+// actions: {
+//     save: function(){
+//       if (this.get('isValid')){
+//         let _this = this;
+//         this.get('dog').save().then(function(dog){
+//           _this.transitionToRoute('dogs.show', dog);
+//         });
+//       } else{
+//         this.set('errorMessage', 'You have to fill all the fields');
+//       }
+//       return false;
+//     },
+//     cancel: function(){
+//       return false;
+//     }
+//   }
+// });
