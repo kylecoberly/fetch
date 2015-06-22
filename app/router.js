d56d2c8eac060072b694aefdaa712af72600bc41
Kyle Coberly <kyle.coberly@gmail.com>
2015-06-22 16:38:56 -0600

    Cleaning up folder structure

--- app/router.js
+++ app/router.js
@@ -2,21 +2,19 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+    location: config.locationType
 });
 
-
-
 Router.map(function() {
-  this.route('users', function() {
-    this.route('new');
-    this.route('show', { path: ':user_id' }, function() {
-      this.route('dogs', function() {
+    this.route('users', function() {
         this.route('new');
-      });
+        this.route('show', { path: ':user_id' }, function() {
+            this.route('dogs', function() {
+                this.route('new');
+            });
+        });
+        this.route('edit', { path: ':user_id/edit' });
     });
-    this.route('edit', { path: ':user_id/edit' });
-  });
 });
 
 export default Router;
--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,6 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'fetch',
      // namespaced directory where resolver will look for your resource files
-     podModulePrefix: 'fetch/pods',
      environment: environment,
      baseURL: '/',
      locationType: 'auto',
@@ -30,11 +29,11 @@ module.exports = function(environment) {
  };
 
  if (environment === 'development') {
-  ENV.APP.LOG_RESOLVER = true;
-  ENV.APP.LOG_ACTIVE_GENERATION = true;
+  //ENV.APP.LOG_RESOLVER = true;
+  //ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    //ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
